ba820ea3518c973f06a37461d56d03fe2a532c81
Denis Kramarenko <cashflow.dkm@gmail.com>
2024-12-25 09:39:27 +1000

    Updated navigation bar with burger-menu

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,7 @@
 "use strict";
+const navContainer = document.getElementById("nav__container");
+const navBurgerMenu = document.getElementById("nav__burger-menu");
+
+navBurgerMenu.addEventListener("click", () => {
+  navContainer.classList.toggle("burger-menu-open");
+})
